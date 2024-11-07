4c6df478c950a27e5c6a3c383f2e602fa7204ff8
andrewantwi <loiskui485@gmail.com>
2024-11-07 08:05:23 +0000

    updates

--- custom_addons/hospital/models/appointment.py
+++ custom_addons/hospital/models/appointment.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from odoo.api import ondelete
 
 
 class HospitalAppointment(models.Model):
@@ -9,13 +10,21 @@ class HospitalAppointment(models.Model):
     _rec_names_search = ['patient_id', 'reference']
 
     reference = fields.Char(string='Reference', default='New')
-    patient_id = fields.Many2one('hospital.patient', string='Patient')
-    date_appointment = fields.Date(string='Appointments')
+    patient_id = fields.Many2one('hospital.patient', string='Patient',ondelete='restrict')
+    date_appointment = fields.Date(string='Date')
     note = fields.Text(string='Note')
     state = fields.Selection([('draft', 'Draft'), ('confirmed', 'Confirmed'),
                               ('ongoing', 'Ongoing'), ('done', 'Done'), ('canceled', 'Canceled')
                               ], default='draft', tracking=True)
-    appointment_line_ids = fields.One2many('hospital.appointment.line','appointment_id',string='Lines')
+    appointment_line_ids = fields.One2many('hospital.appointment.line', 'appointment_id', string='Lines')
+    total_qty = fields.Float(compute='_compute_total_qty', string='Total Quantity', store=True)
+    date_of_birth = fields.Date(related='patient_id.date_of_birth')
+
+    @api.depends('appointment_line_ids','appointment_line_ids.qty')
+    def _compute_total_qty(self):
+        for rec in self:
+            rec.total_qty = sum(rec.appointment_line_ids.mapped('qty'))
+
 
     # Replacing the reference with a sequence generated number
     @api.model_create_multi
@@ -42,6 +51,9 @@ class HospitalAppointment(models.Model):
         for rec in self:
             rec.state = 'canceled'
 
+    def _compute_display_name(self):
+        for rec in self:
+            rec.display_name = f"[{rec.reference}]" f"[{rec.patient_id.name}]"
 
 
 class HospitalAppointmentLine(models.Model):
@@ -49,5 +61,5 @@ class HospitalAppointmentLine(models.Model):
     _description = 'hospital appointment line'
 
     appointment_id = fields.Many2one('hospital.appointment', string='Appointment')
-    product_id = fields.Many2one('product.product', string='Product')
+    product_id = fields.Many2one('product.product', string='Product', required=True)
     qty = fields.Float(string='Quantity')
--- custom_addons/hospital/models/patient.py
+++ custom_addons/hospital/models/patient.py
@@ -1,5 +1,5 @@
 from odoo import api, fields, models
-
+from odoo.exceptions import ValidationError
 
 class Hospital(models.Model):
     _name = 'hospital.patient'
@@ -11,3 +11,14 @@ class Hospital(models.Model):
     gender = fields.Selection([('Male', 'male'), ('Female', 'female')], tracking=True)
     tag_ids = fields.Many2many('patient.tag', 'patient_tag_rel', 'patient_tag_id',
                                'tag_id', string="Tags")
+
+
+# function that runs when you try to delete a record in this model
+    def unlink(self):
+        for rec in self:
+            print('This function is running',rec.name)
+            domain = [('patient_id', '=', rec.id)]
+            appointments = self.env['hospital.appointment'].search(domain)
+            if appointments:
+                raise ValidationError('You cannot delete the patients now,\n Appointments exist for patient:%s' %rec.name)
+        return super().unlink()
