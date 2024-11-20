348272d812ceec3fdc665b44796076f9de2fab69
andrewantwi <loiskui485@gmail.com>
2024-11-20 10:02:09 +0000

    updates

---
+++ custom_addons/trial/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'trial',
+    'version': '1.0',
+    'category': 'Uncategorized',
+    'summary': 'Description of trial',
+    'author': 'Your Name',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/my_model_views.xml',
+        'views/menu.xml',
+        'data/data_file.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
